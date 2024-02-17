srcery-colors/srcery-vim

commit 2e9b1d46bf28cf390950c586d5d1c688a009c8ec

fix: define NormalFloat and FloatBorder
This brings popup windows more in line with each other. The change was
prompted by `ChatGPT.nvim` plugin that use telescope as a dependency, but
the borders looked off.

`NormalFloat` was linked to `Pmenu`, `Pmenu` is used for completion, and
needs to be a different background color than NormalFloat, so I set
NormalFloat to srcery_black

`FloatBorder` was linked via `FloatBorder` -> `WinSeparator` ->
`VertSplit` which is set to `bright_white`, but Telescope uses white as
the border color, so I define that for `FloatBorder` as well

```viml
call s:HL('VertSplit', s:bright_white, g:srcery_bg)
```

colors/srcery.vim
@@ -173,6 +173,9 @@ call s:HL('SrceryXgray6', s:xgray6)
 "
 call s:HL('Normal', s:bright_white, g:srcery_bg)
 
+call s:HL('NormalFloat', s:bright_white, g:srcery_bg)
+call s:HL('FloatBorder', s:white, g:srcery_bg)
+
 if v:version >= 700
   " Screen line that the cursor is
   call s:HL('CursorLine',   s:none, s:xgray2)

commit f2c705e3007f8cb4e740f1fbb41edb3dc4e3a3f0

fix: no contrast issue for lsp popups
Issue was introduced with 2e9b1d4, defining NormalFloat to srcery_bg was
a bad idea and broke other popups, due to lacking background contrast.

I left in the border fix though, so that popup borders match what's
configured for telescope.

This still leaves some plugin borders looking a bit off, especially if
they configure the background to something other than NormalFloat.

Fixes: #115

colors/srcery.vim
@@ -173,7 +173,6 @@ call s:HL('SrceryXgray6', s:xgray6)
 "
 call s:HL('Normal', s:bright_white, g:srcery_bg)
 
-call s:HL('NormalFloat', s:bright_white, g:srcery_bg)
 call s:HL('FloatBorder', s:white, g:srcery_bg)
 
 if v:version >= 700